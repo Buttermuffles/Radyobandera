cc778d4307a85b7b77019e981691369237957604
John Rainville D. Carandang <64280101+Buttermuffles@users.noreply.github.com>
2026-06-23 12:21:17 +0800

    chore: slow ticker to 600s for older users

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,7 +55,7 @@ export default {
         },
       },
       animation: {
-        ticker: "ticker 90s linear infinite",
+        ticker: "ticker 600s linear infinite",
       },
       boxShadow: {
         card: "0 1px 3px rgba(0, 0, 0, 0.1)",
